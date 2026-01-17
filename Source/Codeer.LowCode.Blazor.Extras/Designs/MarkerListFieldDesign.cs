afab3bc6e815cd33c455ca5657707c351dc25c28
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2026-01-17 13:43:47 +0900

    add MarkerListField

---
+++ Source/Codeer.LowCode.Blazor.Extras/Designs/MarkerListFieldDesign.cs
@@ -0,0 +1,56 @@
+﻿using Codeer.LowCode.Blazor.DesignLogic.Check;
+using Codeer.LowCode.Blazor.DesignLogic.Refactor;
+using Codeer.LowCode.Blazor.Extras.Components;
+using Codeer.LowCode.Blazor.Extras.Fields;
+using Codeer.LowCode.Blazor.OperatingModel;
+using Codeer.LowCode.Blazor.Repository.Data;
+using Codeer.LowCode.Blazor.Repository.Design;
+
+namespace Codeer.LowCode.Blazor.Extras.Designs
+{
+    public class MarkerListFieldDesign : FieldDesignBase, IDataDependentField
+    {
+        public MarkerListFieldDesign() : base(typeof(MarkerListFieldDesign).FullName!) { }
+
+        [Designer(CandidateType = CandidateType.Resource)]
+        public string ResourcePath { get; set; } = string.Empty;
+
+        [Designer(CandidateType = CandidateType.Field)]
+        [TargetFieldType(Types = [typeof(ListFieldDesignBase)])]
+        public string ListField { get; set; } = string.Empty;
+
+        [Designer]
+        public string LabelFieldOfListField { get; set; } = string.Empty;
+
+        [Designer(CandidateType = CandidateType.ScriptEvent),
+         ScriptMethod(ArgumentTypes = ["string"], ArgumentNames = ["id"])]
+        public string OnClickMarker { get; set; } = string.Empty;
+
+        [Designer(CandidateType = CandidateType.ScriptEvent),
+         ScriptMethod(ArgumentTypes = ["int", "int"], ArgumentNames = ["x", "y"])]
+        public string OnDoubleClickPoint { get; set; } = string.Empty;
+
+        public override string GetWebComponentTypeFullName() => typeof(MarkerListFieldComponent).FullName!;
+
+        public override string GetSearchWebComponentTypeFullName() => string.Empty;
+
+        public override string GetSearchControlTypeFullName() => string.Empty;
+
+        public override FieldDataBase? CreateData() => null;
+
+        public override FieldBase CreateField() => new MarkerListField(this);
+
+        public override List<DesignCheckInfo> CheckDesign(DesignCheckContext context)
+        {
+            var result = base.CheckDesign(context);
+            context.CheckFieldFieldExistence(Name, nameof(ListField), ListField).AddTo(result);
+            return result;
+        }
+
+        public override RenameResult ChangeName(RenameContext context) => context.Builder(base.ChangeName(context))
+            .AddField(ListField, x => ListField = x)
+            .Build();
+
+        public List<string> GetDependencyFields() => string.IsNullOrEmpty(ListField) ? new() : [ListField];
+    }
+}
---
+++ Source/Codeer.LowCode.Blazor.Extras/Fields/MarkerListField.cs
@@ -0,0 +1,95 @@
+﻿using Codeer.LowCode.Blazor.DataIO;
+using Codeer.LowCode.Blazor.Extras.Designs;
+using Codeer.LowCode.Blazor.OperatingModel;
+using Codeer.LowCode.Blazor.Repository.Data;
+using Codeer.LowCode.Blazor.Script;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.JSInterop;
+
+namespace Codeer.LowCode.Blazor.Extras.Fields
+{
+    public class Marker 
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Label { get; set; } = string.Empty;
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+    public class MarkerListField : FieldBase<MarkerListFieldDesign>
+    {
+        ListField? _list;
+
+        internal string ObjectUrl { get; set; } = string.Empty;
+        internal string ImageExtension => Design.ResourcePath.Split('.').LastOrDefault()??string.Empty;
+        internal List<Marker> MarkerList { get; set; } = new();
+
+        public MarkerListField(MarkerListFieldDesign design) : base(design) { }
+        public override bool IsModified => false;
+        public override FieldDataBase? GetData() => null;
+        public override FieldSubmitData GetSubmitData() => new();
+        public override Task SetDataAsync(FieldDataBase? fieldDataBase) => Task.CompletedTask;
+
+        [ScriptHide]
+        public override async Task InitializeDataAsync(FieldDataBase? fieldDataBase)
+        {
+            await LoadImage();
+        }
+
+        internal async Task InitAsync()
+        {
+            await Task.CompletedTask;
+            _list = Module.GetField<ListField>(Design.ListField);
+
+            var js = Module.Services.Provider.GetService<IJSRuntime>();
+
+            if (_list != null)
+            {
+                _list.OnDataChangedAsync = async () =>
+                {
+                    InitList();
+                    await Task.CompletedTask;
+                    NotifyStateChanged();
+                };
+            }
+            InitList();
+        }
+
+        internal async Task OnDoubleClickPointAsync(int x, int y)
+            => await Module!.ExecuteScriptAsync(Design.OnDoubleClickPoint, x, y);
+
+        internal async Task OnClickMarkerAsync(string id)
+            => await Module!.ExecuteScriptAsync(Design.OnClickMarker, id);
+
+        void InitList()
+        {
+            if (Module.Services.AppInfoService.IsDesignMode) return;
+
+            MarkerList = new();
+            if (_list == null) return;
+
+            foreach (var e in _list.Rows)
+            {
+                var x = e.GetFields();
+
+                var marker = new Marker();
+                marker.Id = e.GetField<IdField>("Id")?.Value ?? string.Empty;
+                marker.Label = e.GetField<TextField>(Design.LabelFieldOfListField)?.Value ?? string.Empty;
+                marker.X = (int)(e.GetField<NumberField>("X")?.Value ?? 0);
+                marker.Y = (int)(e.GetField<NumberField>("Y")?.Value ?? 0);
+                MarkerList.Add(marker);
+            }
+        }
+
+        async Task LoadImage()
+        {
+            if (string.IsNullOrEmpty(Design.ResourcePath)) return;
+
+            using var memoryStream = await Services.AppInfoService.GetResourceAsync(Design.ResourcePath);
+            if (memoryStream == null) return;
+
+            var bin = memoryStream!.ToArray();
+            ObjectUrl = Convert.ToBase64String(bin);
+        }
+    }
+}
