226a71f52061df83e73eba6a2947a1d91fc6506a
石川達也 <ishikawa-tatsuya@codeer.co.jp>
2026-03-10 19:59:48 +0900

    MarkerList調整

--- Source/Codeer.LowCode.Blazor.Extras/Designs/MarkerListFieldDesign.cs
+++ Source/Codeer.LowCode.Blazor.Extras/Designs/MarkerListFieldDesign.cs
@@ -1,34 +1,45 @@
-﻿using Codeer.LowCode.Blazor.DesignLogic.Check;
+using Codeer.LowCode.Blazor.DesignLogic;
+using Codeer.LowCode.Blazor.DesignLogic.Check;
 using Codeer.LowCode.Blazor.DesignLogic.Refactor;
 using Codeer.LowCode.Blazor.Extras.Components;
 using Codeer.LowCode.Blazor.Extras.Fields;
 using Codeer.LowCode.Blazor.OperatingModel;
 using Codeer.LowCode.Blazor.Repository.Data;
 using Codeer.LowCode.Blazor.Repository.Design;
+using Codeer.LowCode.Blazor.Repository.Match;
 
 namespace Codeer.LowCode.Blazor.Extras.Designs
 {
-    public class MarkerListFieldDesign : FieldDesignBase, IDataDependentField
+    public class MarkerListFieldDesign() : FieldDesignBase(typeof(MarkerListFieldDesign).FullName!),
+        ISearchResultsViewFieldDesign
     {
-        public MarkerListFieldDesign() : base(typeof(MarkerListFieldDesign).FullName!) { }
-
         [Designer(CandidateType = CandidateType.Resource)]
         public string ResourcePath { get; set; } = string.Empty;
 
+        [Designer(Scope = DesignerScope.All)]
+        public SearchCondition SearchCondition { get; set; } = new();
+
+        [Designer(CandidateType = CandidateType.DetailLayout)]
+        [Layout(ModuleNameMember = $"{nameof(SearchCondition)}.{nameof(SearchCondition.ModuleName)}")]
+        public string DetailLayoutName { get; set; } = string.Empty;
+
         [Designer(CandidateType = CandidateType.Field)]
-        [TargetFieldType(Types = [typeof(ListFieldDesignBase)])]
-        public string ListField { get; set; } = string.Empty;
+        [ModuleMember(Member = $"{nameof(SearchCondition)}.{nameof(SearchCondition.ModuleName)}")]
+        [TargetFieldType(Types = [typeof(NumberFieldDesign)])]
+        public string XField { get; set; } = string.Empty;
 
-        [Designer]
-        public string LabelFieldOfListField { get; set; } = string.Empty;
+        [Designer(CandidateType = CandidateType.Field)]
+        [ModuleMember(Member = $"{nameof(SearchCondition)}.{nameof(SearchCondition.ModuleName)}")]
+        [TargetFieldType(Types = [typeof(NumberFieldDesign)])]
+        public string YField { get; set; } = string.Empty;
 
-        [Designer(CandidateType = CandidateType.ScriptEvent),
-         ScriptMethod(ArgumentTypes = ["string"], ArgumentNames = ["id"])]
-        public string OnClickMarker { get; set; } = string.Empty;
+        [Designer(CandidateType = CandidateType.Field)]
+        [ModuleMember(Member = $"{nameof(SearchCondition)}.{nameof(SearchCondition.ModuleName)}")]
+        [TargetFieldType(Types = [typeof(TextFieldDesign)])]
+        public string LabelField { get; set; } = string.Empty;
 
-        [Designer(CandidateType = CandidateType.ScriptEvent),
-         ScriptMethod(ArgumentTypes = ["int", "int"], ArgumentNames = ["x", "y"])]
-        public string OnDoubleClickPoint { get; set; } = string.Empty;
+        [Designer(CandidateType = CandidateType.ScriptEvent)]
+        public string OnDataChanged { get; set; } = string.Empty;
 
         public override string GetWebComponentTypeFullName() => typeof(MarkerListFieldComponent).FullName!;
 
@@ -42,15 +53,22 @@ namespace Codeer.LowCode.Blazor.Extras.Designs
 
         public override List<DesignCheckInfo> CheckDesign(DesignCheckContext context)
         {
-            var result = base.CheckDesign(context);
-            context.CheckFieldFieldExistence(Name, nameof(ListField), ListField).AddTo(result);
+            var result = new List<DesignCheckInfo>();
+            context.CheckFieldRelativeFieldExistence(Name, nameof(XField), SearchCondition.ModuleName, XField).AddTo(result);
+            context.CheckFieldRelativeFieldExistence(Name, nameof(YField), SearchCondition.ModuleName, YField).AddTo(result);
+            context.CheckFieldRelativeFieldExistence(Name, nameof(LabelField), SearchCondition.ModuleName, LabelField).AddTo(result);
+            context.CheckFieldRelativeModuleLayoutExistence(Name, nameof(DetailLayoutName), SearchCondition.ModuleName, DetailLayoutName).AddTo(result);
+            context.CheckFieldFunctionExistence(Name, nameof(OnDataChanged), OnDataChanged, null).AddTo(result);
+            result.AddRange(SearchCondition.CheckDesign(context, Name, nameof(SearchCondition)));
             return result;
         }
 
         public override RenameResult ChangeName(RenameContext context) => context.Builder(base.ChangeName(context))
-            .AddField(ListField, x => ListField = x)
+            .AddField(SearchCondition.ModuleName, XField, x => XField = x)
+            .AddField(SearchCondition.ModuleName, YField, x => YField = x)
+            .AddField(SearchCondition.ModuleName, LabelField, x => LabelField = x)
+            .AddLayout(SearchCondition.ModuleName, ModuleLayoutType.Detail, DetailLayoutName, x => DetailLayoutName = x)
+            .AddMatchCondition(SearchCondition)
             .Build();
-
-        public List<string> GetDependencyFields() => string.IsNullOrEmpty(ListField) ? new() : [ListField];
     }
 }
--- Source/Codeer.LowCode.Blazor.Extras/Fields/MarkerListField.cs
+++ Source/Codeer.LowCode.Blazor.Extras/Fields/MarkerListField.cs
@@ -1,28 +1,36 @@
-﻿using Codeer.LowCode.Blazor.DataIO;
+using Codeer.LowCode.Blazor.DataIO;
+using Codeer.LowCode.Blazor.DesignLogic;
 using Codeer.LowCode.Blazor.Extras.Designs;
 using Codeer.LowCode.Blazor.OperatingModel;
 using Codeer.LowCode.Blazor.Repository.Data;
+using Codeer.LowCode.Blazor.Repository.Design;
+using Codeer.LowCode.Blazor.Repository.Match;
 using Codeer.LowCode.Blazor.Script;
+using Codeer.LowCode.Blazor.Script.Internal.ScriptServices;
 
 namespace Codeer.LowCode.Blazor.Extras.Fields
 {
-    public class Marker 
+    public class Marker
     {
         public string Id { get; set; } = string.Empty;
         public string Label { get; set; } = string.Empty;
         public int X { get; set; }
         public int Y { get; set; }
+        public Module? Module { get; set; }
     }
 
-    public class MarkerListField : FieldBase<MarkerListFieldDesign>
+    public class MarkerListField(MarkerListFieldDesign design)
+        : FieldBase<MarkerListFieldDesign>(design), ISearchResultsViewField
     {
-        ListField? _list;
+        private SearchCondition? _additionalCondition;
 
         internal string ObjectUrl { get; set; } = string.Empty;
-        internal string ImageExtension => Design.ResourcePath.Split('.').LastOrDefault()??string.Empty;
+        internal string ImageExtension => Design.ResourcePath.Split('.').LastOrDefault() ?? string.Empty;
         internal List<Marker> MarkerList { get; set; } = new();
 
-        public MarkerListField(MarkerListFieldDesign design) : base(design) { }
+        [ScriptHide]
+        public string ModuleName => Design?.SearchCondition.ModuleName ?? string.Empty;
+
         public override bool IsModified => false;
         public override FieldDataBase? GetData() => null;
         public override FieldSubmitData GetSubmitData() => new();
@@ -32,52 +40,125 @@ namespace Codeer.LowCode.Blazor.Extras.Fields
         public override async Task InitializeDataAsync(FieldDataBase? fieldDataBase)
         {
             await LoadImage();
+            if (this.IsInLayout()) await ReloadAsync();
         }
 
-        internal async Task InitAsync()
+        [ScriptName("SetAdditionalCondition")]
+        public async Task SetAdditionalConditionAsync(ModuleSearcher searcher)
+            => await SetAdditionalConditionAsync(searcher.GetSearchCondition(), 0);
+
+        [ScriptHide]
+        public async Task SetAdditionalConditionAsync(SearchCondition condition, int page)
         {
             await Task.CompletedTask;
-            _list = Module.GetField<ListField>(Design.ListField);
-
-            if (_list != null)
-            {
-                _list.OnDataChangedAsync = async () =>
-                {
-                    InitList();
-                    await Task.CompletedTask;
-                    NotifyStateChanged();
-                };
-            }
-            InitList();
+            if (condition.ModuleName != Design.SearchCondition.ModuleName)
+                throw LowCodeException.Create("{0} Invalid Module", Design.SearchCondition.ModuleName, condition.ModuleName);
+            _additionalCondition = condition;
         }
 
-        internal async Task OnDoubleClickPointAsync(int x, int y)
-            => await Module!.ExecuteScriptAsync(Design.OnDoubleClickPoint, x, y);
-
-        internal async Task OnClickMarkerAsync(string id)
-            => await Module!.ExecuteScriptAsync(Design.OnClickMarker, id);
-
-        void InitList()
+        [ScriptHide]
+        public override async Task OnExternalFieldChangedAsync(string fieldName)
         {
-            if (Module.Services.AppInfoService.IsDesignMode) return;
-
-            MarkerList = new();
-            if (_list == null) return;
-
-            foreach (var e in _list.Rows)
-            {
-                var x = e.GetFields();
-
-                var marker = new Marker();
-                marker.Id = e.GetField<IdField>("Id")?.Value ?? string.Empty;
-                marker.Label = e.GetField<TextField>(Design.LabelFieldOfListField)?.Value ?? string.Empty;
-                marker.X = (int)(e.GetField<NumberField>("X")?.Value ?? 0);
-                marker.Y = (int)(e.GetField<NumberField>("Y")?.Value ?? 0);
-                MarkerList.Add(marker);
-            }
+            if (!this.IsInLayout()) return;
+            var searchCondition = GetSearchCondition();
+            if (searchCondition.GetFieldVariableConditions().All(e => new VariableName(e.Variable).FieldName.Root != fieldName)) return;
+            await ReloadAsync();
         }
 
-        async Task LoadImage()
+        [ScriptHide]
+        public override async Task OnChildDataChangedAsync()
+        {
+            await Task.CompletedTask;
+            var count = MarkerList.Count;
+            MarkerList.RemoveAll(e => e.Module?.IsDeleted == true);
+            if (count == MarkerList.Count) return;
+
+            await InvokeOnDataChangedAsync();
+            await NotifyDataChangedAsync();
+        }
+
+        [ScriptName("Reload")]
+        public async Task ReloadAsync()
+        {
+            var modules = await this.GetChildModulesAsync(GetSearchCondition(), ModuleLayoutType.Detail, Design.DetailLayoutName);
+            MarkerList.Clear();
+            MarkerList.AddRange(modules.Select(ConvertToMarker));
+            NotifyStateChanged();
+        }
+
+        internal async Task AddAsync(int x, int y)
+        {
+            var mod = await this.CreateChildModuleAsync(ModuleName, ModuleLayoutType.Detail, Design.DetailLayoutName);
+
+            await mod.AssignRequiredCondition(Design.SearchCondition);
+
+            SetNumberFieldValue(mod, Design.XField, x);
+            SetNumberFieldValue(mod, Design.YField, y);
+
+            if (await mod.ShowDialogAsync(Properties.Resources.OK, Properties.Resources.Cancel) != Properties.Resources.OK) return;
+            if (!mod.ValidateInput())
+            {
+                await Services.UIService.NotifyError(Properties.Resources.InputError);
+                return;
+            }
+            if (await mod.SubmitAsync() != true) return;
+
+            await ReloadAsync();
+            await InvokeOnDataChangedAsync();
+            await NotifyDataChangedAsync();
+        }
+
+        internal async Task EditAsync(Module? mod)
+        {
+            if (mod == null) return;
+
+            var dialogResult = await mod.ShowDialogAsync(Properties.Resources.Update, Properties.Resources.Delete, Properties.Resources.Cancel);
+            if (dialogResult == Properties.Resources.Update)
+            {
+                if (!mod.ValidateInput())
+                {
+                    await Services.UIService.NotifyError(Properties.Resources.InputError);
+                    return;
+                }
+                if (await mod.SubmitAsync() != true) return;
+            }
+            else if (dialogResult == Properties.Resources.Delete)
+            {
+                await mod.DeleteAsync();
+            }
+            else
+            {
+                return;
+            }
+
+            await ReloadAsync();
+            await InvokeOnDataChangedAsync();
+            await NotifyDataChangedAsync();
+        }
+
+        private async Task InvokeOnDataChangedAsync()
+        {
+            await Module.ExecuteScriptAsync(Design.OnDataChanged);
+        }
+
+        private Marker ConvertToMarker(Module data)
+        {
+            return new Marker
+            {
+                Id = data.GetIdText(),
+                Label = string.IsNullOrEmpty(Design.LabelField)
+                    ? string.Empty
+                    : data.GetField<TextField>(Design.LabelField)?.Value ?? string.Empty,
+                X = (int)(data.GetField<NumberField>(Design.XField)?.Value ?? 0),
+                Y = (int)(data.GetField<NumberField>(Design.YField)?.Value ?? 0),
+                Module = data,
+            };
+        }
+
+        private SearchCondition GetSearchCondition()
+            => Design.SearchCondition.MergeSearchCondition(_additionalCondition);
+
+        private async Task LoadImage()
         {
             if (string.IsNullOrEmpty(Design.ResourcePath)) return;
 
@@ -86,6 +167,13 @@ namespace Codeer.LowCode.Blazor.Extras.Fields
 
             var bin = memoryStream!.ToArray();
             ObjectUrl = Convert.ToBase64String(bin);
+        }
+
+        private static void SetNumberFieldValue(Module mod, string fieldName, int value)
+        {
+            if (string.IsNullOrEmpty(fieldName)) return;
+            var field = mod.GetField<NumberField>(fieldName);
+            field?.SetValueAsync((decimal)value).GetAwaiter().GetResult();
         }
     }
 }
